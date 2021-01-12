678b5bed2275f730f5411ef44838d4f6dea036de
Ketki Kulkarni <ketki.kulkarni@theperformancestencil.com>
2021-01-12 14:20:31 +0530

    aaa

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ let express = require('express');
 let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io').listen(server);
-
 let stream = require('./ws/stream');
 let path = require('path');
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000);
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res)=>{
